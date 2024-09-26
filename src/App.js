4d52d44e596f526b2f136c98377524b1196c3de3
Anuj Tomar <anuj.tomar@nashtechglobal.com>
2024-09-26 14:45:16 +0530

    react star rating

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  const [totalStars, setTotalStars] = useState(5);
+
+  const handleChange = (e) => {
+    setTotalStars(parseInt(Boolean(e.target.value, 10) ? e.target.value : 5));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star rating</h1>
+      {[...Array(totalStars)].map((star, index) => {
+        const currentRating = index + 1;
+
+        return (
+          <label key={index}>
+            <input
+              key={star}
+              type="radio"
+              name="rating"
+              value={currentRating}
+              onChange={() => setRating(currentRating)}
+            />
+            <span
+              className="star"
+              style={{
+                color:
+                  currentRating <= (hover || rating) ? "#ffc107" : "#e4e5e9",
+              }}
+              onMouseEnter={() => setHover(currentRating)}
+              onMouseLeave={() => setHover(null)}
+            >
+              &#9733;
+            </span>
+          </label>
+        );
+      })}
+      <br />
+      <br />
+      <p>Your rating is: {rating}</p>
+      <br />
+      <label style={{ fontWeight: 400 }}>
+        Number of stars:
+        <input
+          style={{ marginLeft: "12px", maxWidth: "50px" }}
+          onChange={handleChange}
+          value={totalStars}
+          type="number"
+          min={1}
+        />
+      </label>
     </div>
   );
 }
-
-export default App;
